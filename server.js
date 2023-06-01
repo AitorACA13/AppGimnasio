4b460e994873bc8df82bd37dcf1b0ad9ae208ab5
Ryan John Quinn <ryanquinn23@live.co.uk>
2023-06-01 17:25:49 +0200

    levantar server Ryan

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+//error middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(err.httpStatus || 500).send({
+    status: 'error',
+    message: err.message,
+  });
+});
+
+//path not found
+app.use((req, res) => {
+  res.status(404).send({});
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at http://localhost:${process.env.PORT}`);
+});

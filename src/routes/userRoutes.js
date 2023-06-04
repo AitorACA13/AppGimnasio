7ed86029b684bbffda421a8f080d3673f473f08d
Marina Hernandez <marina.hernandez@LECOL-044.local>
2023-06-04 11:44:16 +0100

    actualizacion

--- src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { newUser } = require('../../controllers/users/index');
-
-router.post('/users', newUser);
-
-module.exports = router;
